624f8fa46e88906d22f776ef15ae3509af3c27c2
inameJay <2424994234@qq.com>
2016-05-09 15:42:04 +0800

    nsf_master.c 	nsf_master.h 	nsf_worker.c

--- nsf_signal.c
+++ nsf_signal.c
@@ -1,13 +1,17 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include "nsf_epoll.h"
+#include "nsf_master.h"
 #include <stdio.h>
+#include <stdlib.h>
 int exit_flag = 0;
 
 void nsf_signal_handler(int signo)
 {
 	pid_t pid;
 	int stat;
+	struct nsf_notification_message msg;
+	memset(&msg, 0, sizeof(msg));
 	switch(signo){
 	case SIGCHLD:
 		while((pid=waitpid(-1,&stat,WNOHANG))>0)
@@ -15,7 +19,12 @@ void nsf_signal_handler(int signo)
 		break;
 	case SIGINT:
 		//退出前先卸载有名信号量
+		printf("卸载信号量\n");
 		nsf_unlink_sem();
+		msg.message = NM_CLOSE;
+		printf("通知子进程关闭\n");
+		nsf_notification(0, msg);
+		sleep(1);
 		exit(0);
 		break;
 	default:
