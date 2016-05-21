f18452a6c33dd4e2cf5f1869c6f16ce853a4131e
CrystalGrape <2424994234@qq.com>
2016-05-22 00:03:20 +0800

    修改:         modules/Makefile 	修改:         modules/module_init.c

--- modules/module_init.c
+++ modules/module_init.c
@@ -1,12 +1,15 @@
 #include "module.h"
-extern void nsf_talk_proc(int event, NsfntPkg pkg);
+//extern void nsf_talk_proc(int event, NsfntPkg pkg);
+extern void nsf_chess_proc(int event, NsfntPkg pkg);
 void nsf_module_init()
 {
+	/*
 	nsf_register_event(NE_ACPT, nsf_talk_proc);
 	nsf_register_event(NE_RECV, nsf_talk_proc);
 	nsf_register_event(NE_QUIT, nsf_talk_proc);
+	*/	
+	nsf_register_event(NE_ACPT, nsf_chess_proc);
+	nsf_register_event(NE_RECV, nsf_chess_proc);
+	nsf_register_event(NE_QUIT, nsf_chess_proc);
 }
 
-void nsf_master_module_init()
-{
-}
