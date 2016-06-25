de83b2f7c9545cf1fbb952349bdc1831fb26bb6c
jay <jay@jay-virtual-machine.(none)>
2016-06-25 14:41:01 +0800

    deleted:    insertdata.sh 	deleted:    modules/module.h 	deleted:    modules/module_init.c 	deleted:    modules/talkserver/avl.c 	deleted:    modules/talkserver/avl.h 	deleted:    modules/talkserver/types.h

--- insertdata.sh
+++
@@ -1,5 +0,0 @@
-query="insert into node_data values(\"${1}\",${2},now())"
-echo $query
-mysql -uroot -pinameJay gcsj -e"$query"
-
-
--- modules/module.h
+++
@@ -1,8 +0,0 @@
-#ifndef module_h
-#define module_h
-#include "../nsf_config.h"
-#include "../nsf_epoll.h"
-#include "../nsf_event.h"
-#include "../nsf_master.h"
-#include "../nsf_signal.h"
-#endif
